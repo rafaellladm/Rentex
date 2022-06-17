ef80fc4996334890dbadcccbf2985efeb498ee0e
Rafael Luz <rafaellladm@gmail.com>
2022-06-17 20:55:54 -0300

    feat: Creation of validation if category already exists

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -8,6 +8,12 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post("/", (req, res) => {
   const { name, description } = req.body;
 
+  const categoriesAlreadyExists = categoriesRepository.findByName(name);
+
+  if (categoriesAlreadyExists) {
+    return res.status(400).json({ error: "Category already exists!" });
+  }
+
   categoriesRepository.create({ name, description });
 
   return res.status(201).send();
