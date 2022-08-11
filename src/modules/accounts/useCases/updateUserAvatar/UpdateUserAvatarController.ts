4d44f52ccb0ead14275b1c40b92324dd5275c4c2
Rafael Luz <rafaellladm@gmail.com>
2022-08-10 21:10:21 -0300

    fix: import and upload file request

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController.ts
@@ -1,19 +1,18 @@
-import { Request, response, Response } from "express";
+import { Request, Response } from "express";
 import { container } from "tsyringe";
 
 import { UpdateUserAvatarUseCase } from "./UpdateUserAvatarUseCase";
 
 class UpdateUserAvatarController {
-  async handle(req: Request, res: Response) {
+  async handle(req: Request, res: Response): Promise<Response> {
     const { id } = req.user;
-
-    const avatar_file = null;
+    const avatar_file = req.file.filename;
 
     const updateUserAvatarUseCase = container.resolve(UpdateUserAvatarUseCase);
 
     await updateUserAvatarUseCase.execute({ user_id: id, avatar_file });
 
-    return response.status(200).send();
+    return res.status(200).send();
   }
 }
 
