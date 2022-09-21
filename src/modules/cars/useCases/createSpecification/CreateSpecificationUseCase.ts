646bee5fba379ff23b6b15503993575512eb77cf
Rafael Luz <rafaellladm@gmail.com>
2022-09-20 23:47:48 -0300

    refactor: import mapping

--- src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -1,7 +1,7 @@
 import { inject, injectable } from "tsyringe";
 
-import { AppError } from "../../../../errors/AppError";
-import { ISpacificationRepository } from "../../repositories/ISpecificationRepository";
+import { AppError } from "@errors/AppError";
+import { ISpacificationRepository } from "@modules/cars/repositories/ISpecificationRepository";
 
 interface IRequest {
   name: string;
